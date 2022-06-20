d79a2d77f8a3676b1a07111c9d4abcec6ac09837
Davi Freitas <df828316@gmail.com>
2022-06-20 17:35:03 -0300

    criado arr e pessoas cadatras e parte da função handleTalkerRequest

--- index.js
+++ index.js
@@ -12,6 +12,42 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
-});
+  app.listen(PORT, () => {
+    console.log('Online');
+  });
+// requisito 1
+// PPC = Pessoas Palestrantes Cadastradas
+const arrPPC = [
+  { name: 'Henrique Albuquerque',
+    age: 62,
+    id: 1,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Heloísa Albuquerque',
+    age: 67,
+    id: 2,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Ricardo Xavier Filho',
+    age: 33,
+    id: 3,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+  {
+    name: 'Marcos Costa',
+    age: 24,
+    id: 4,
+    talk: { watchedAt: '23/10/2020', rate: 5 },
+  },
+];
+function handleTalkerRequest(req, res) {
+  // const { id } = req.params;
+  // const pessoa = arrPPC.find((p) => p.id === Number(id));
+
+  if (arrPPC.length === 0) return res.status(200).send([]);
+  if (arrPPC) return res.status(200).send(arrPPC);
+}
+
+app.get('/talker', handleTalkerRequest);
