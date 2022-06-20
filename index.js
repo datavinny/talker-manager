1ea0679d9575ecf7ca6d68054330ceebf1d53ad8
Davi Freitas <df828316@gmail.com>
2022-06-20 18:28:42 -0300

    validação login

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (_request, response) => {
   app.listen(PORT, () => {
     console.log('Online');
   });
-// requisito 1
+// GetAllTalkersRequest
 // PPC = Pessoas Palestrantes Cadastradas
 const arrPPC = [
   { name: 'Henrique Albuquerque',
@@ -48,7 +48,7 @@ function handleGetAllTalkersRequest(req, res) {
 }
 
 app.get('/talker', handleGetAllTalkersRequest);
-
+// GetTalkerByIdRequest
 function handleGetTalkerByIdRequest(req, res) {
   const { id } = req.params;
   const pessoa = arrPPC.find((p) => p.id === Number(id));
@@ -60,3 +60,48 @@ function handleGetTalkerByIdRequest(req, res) {
 }
 
 app.get('/talker/:id', handleGetTalkerByIdRequest);
+// endpoint POST /login
+const credenciais = {
+  email: 'email@email.com',
+  password: '123456',
+};
+
+const mensagensLogin = {
+1: { message: 'O campo "email" é obrigatório' },
+2: { message: 'O "email" deve ter o formato "email@email.com"' },
+3: { message: 'O campo "password" é obrigatório' },
+4: { message: 'O "password" deve ter pelo menos 6 caracteres' },
+5: 'pass',
+};
+
+const verificarCredenciais = (email, password) => {
+  const mailformat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // const authEmail = credenciais.email === email;
+  // const authPassword = credenciais.password === password;
+  // const auth = !!(authEmail && authPassword === true);
+  if (email === undefined) {
+    return mensagensLogin[1]; 
+  }
+  if (email.match(mailformat) === null) {
+    return mensagensLogin[2];
+  }
+  if (password === undefined) {
+    return mensagensLogin[3]; 
+  }
+  if (password.length < 6) {
+    return mensagensLogin[4];  
+  }
+  return mensagensLogin[5];
+};
+
+function handleLogin(req, res) {
+  const { email, password } = req.body;
+
+  const authMessage = verificarCredenciais(email, password);
+  const token = '7mqaVRXJSp886CGr'; // token aleatório a cada vez que for acessado.
+  
+  if (authMessage === 'pass') return res.status(200).send({ token });
+  return res.status(400).send(authMessage);
+}
+
+app.post('/login', handleLogin);
